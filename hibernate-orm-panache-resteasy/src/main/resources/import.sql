bb9d54ce021780e3219f74f508815717a732fc2f
Dmytro Prytula <prituladima@gmail.com>
2019-07-24 02:07:26 +0300

    saved pro

--- hibernate-orm-panache-resteasy/src/main/resources/import.sql
+++ hibernate-orm-panache-resteasy/src/main/resources/import.sql
@@ -1,3 +1,6 @@
-INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Cherry')
-INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Apple')
-INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Banana')
+-- DROP TABLE IF EXISTS FRUIT
+-- CREATE TABLE FRUIT
+INSERT INTO Fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Cherry')
+INSERT INTO Fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Apple')
+INSERT INTO Fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Banana')
+-- show tables
--- hibernate-orm-panache-resteasy/src/test/java/org/acme/hibernate/orm/panache/FruitsEndpointTest.java
+++ hibernate-orm-panache-resteasy/src/test/java/org/acme/hibernate/orm/panache/FruitsEndpointTest.java
@@ -4,10 +4,14 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.core.IsNot.not;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+
 @QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
 public class FruitsEndpointTest {
 
     @Test
