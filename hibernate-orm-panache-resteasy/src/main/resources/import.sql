694426afac8140e266331d9146008516539ceb00
Dmytro Prytula <prituladima@gmail.com>
2019-07-24 02:23:33 +0300

    saved pro

--- hibernate-orm-panache-resteasy/src/main/resources/import.sql
+++ hibernate-orm-panache-resteasy/src/main/resources/import.sql
@@ -1,6 +1,4 @@
--- DROP TABLE IF EXISTS FRUIT
--- CREATE TABLE FRUIT
 INSERT INTO Fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Cherry')
 INSERT INTO Fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Apple')
 INSERT INTO Fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Banana')
--- show tables
+INSERT INTO Fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Orange')
